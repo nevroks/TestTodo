ec98bd22e18d767c493e9cdc15ed91feda3db010
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-22 23:26:36 +0500

    added all components to homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import classes from "./style.module.css";
+import {useAppSelector} from "../../hooks/ReduxHooks.ts";
+import React, {useState} from "react";
+import TodoList from "../../components/todo/TodoList.tsx";
+import PopUp from "../../components/popup/PopUp.tsx";
+import {Button, Input} from "@mui/material";
+import CreateTodoForm from "../../components/createtodoform/CreateTodoForm.tsx";
+import ToolBar from "../../components/toolbar/ToolBar.tsx";
 
 const HomePage = () => {
+
+    const todosList=useAppSelector(state => state.todos)
+    const [isCreateFormShowed,setSIsCreateFormShowed]=useState(false)
+
+    function handleCreate() {
+        setSIsCreateFormShowed(true)
+    }
+
     return (
-        <div>
-            
+        <div className={classes.page__content}>
+            <div className={classes.home_navigation}>
+                <Button onClick={handleCreate}>Добавить новую задачу</Button>
+            </div>
+            <br/>
+            <TodoList todos={todosList}/>
+            {isCreateFormShowed && <PopUp setterFunc={setSIsCreateFormShowed}>
+                <CreateTodoForm/>
+            </PopUp>}
+            <ToolBar/>
         </div>
     );
 };
