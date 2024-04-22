04ceaf9642288e623bd56b35a5eb185b0227ef8b
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-22 23:25:54 +0500

    created todoItem

---
+++ src/components/todo/TodoItem.tsx
@@ -0,0 +1,59 @@
+import React, {FC, useEffect, useMemo, useRef, useState} from 'react';
+import {Button, Checkbox, Collapse, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {ExpandLess, ExpandMore, StarBorder} from "@mui/icons-material";
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import PopUp from "../popup/PopUp.tsx";
+import CreateUnderTaskForm from "../createundertaskform/CreateUnderTaskForm.tsx";
+import {useAppDispatch, useAppSelector} from "../../hooks/ReduxHooks.ts";
+import {addSelectedTodo, deleteSelectedTodo} from "../../store/slices/selectedTodosSlice.ts";
+
+type TodoItemProps={
+    todo:ITodo
+}
+
+const TodoItem: FC<TodoItemProps> = ({todo}) => {
+    const [open,setOpen]=useState(false)
+    const [isPopUpShown,setIsPopUpShown]=useState(false)
+    const selectedArr=useAppSelector(state => state.selectedTodos)
+
+    const dispatch=useAppDispatch()
+    const handleClick=()=>{
+        setOpen(prevState => !prevState)
+    }
+    const handleCreate=()=>{
+        setIsPopUpShown(true)
+    }
+    const isTodoSelected=useMemo(()=>{
+        return selectedArr.includes(todo)
+    },[selectedArr])
+    const handleCheckBox=()=>{
+        if (!isTodoSelected){
+            dispatch(addSelectedTodo(todo))
+        }else{
+            dispatch(deleteSelectedTodo(todo))
+        }
+    }
+    return (
+        <div>
+            {isPopUpShown && <PopUp setterFunc={setIsPopUpShown}>
+                <CreateUnderTaskForm todo={todo}/>
+            </PopUp>}
+            <ListItemButton onClick={handleClick}>
+                <Checkbox checked={isTodoSelected} onChange={handleCheckBox} onClick={e=>e.stopPropagation()}/>
+                <ListItemText primary={todo.title} />
+                <ListItemText secondary={todo.description} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <Button onClick={handleCreate}>Создать подзадачу</Button>
+                {/*<List component="div" disablePadding>*/}
+                {/*    <ListItemButton sx={{ pl: 4 }}>*/}
+                {/*        <ListItemText primary="Starred" />*/}
+                {/*    </ListItemButton>*/}
+                {/*</List>*/}
+            </Collapse>
+        </div>
+    );
+};
+
+export default TodoItem;
