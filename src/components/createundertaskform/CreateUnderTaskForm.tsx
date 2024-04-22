d5b48aa81069566887d67b73a1be70badb392098
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-22 23:25:09 +0500

    created form component for creating new under todo

---
+++ src/components/createundertaskform/CreateUnderTaskForm.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import {Button, Input, Stack} from "@mui/material";
+import classes from "./style.module.css";
+import {useAppDispatch} from "../../hooks/ReduxHooks.ts";
+import { addUnderTask} from "../../store/slices/todosSlice.ts";
+
+const CreateUnderTaskForm = ({todo}) => {
+    const dispatch=useAppDispatch()
+    const [newTodo,setNewTodo]=useState({
+        title:'',
+        description:'',
+        underTasks:[]
+    })
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+    }
+    const handleClick=()=>{
+        dispatch(addUnderTask({task:todo,underTask:newTodo}))
+    }
+    return (
+        <form className={classes.create__form} onSubmit={handleSubmit}>
+            <p>Форма создания новой задачи</p>
+            <Stack spacing={2} direction="row">
+                <Input
+                    value={newTodo.title}
+                    onChange={(e)=>setNewTodo({...newTodo,title:e.target.value})}
+                    aria-label="Demo input"
+                    placeholder="Заголовок" />
+                <Input
+                    value={newTodo.description}
+                    onChange={(e)=>setNewTodo({...newTodo,description:e.target.value})}
+                    aria-label="Demo input"
+                    placeholder="Описание" />
+            </Stack>
+            <Button onClick={handleClick}>Создать подзадачу</Button>
+        </form>
+    );
+};
+
+export default CreateUnderTaskForm;
