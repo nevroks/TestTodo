6d165d2fe45ab8a4d24a1860b73669c62e9abb8d
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-22 23:23:08 +0500

    created slice for todos

--- src/store/slices/todosSlice.ts
+++ src/store/slices/todosSlice.ts
@@ -1,18 +1,30 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-const initialState = []
+const initialState:Array<ITodo> = []
 
 export const todosSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
-        addTodo:(state,{payload})=>{
+        addTodo:(state,{payload}:PayloadAction<ITodo>)=>{
             state.push(payload)
+        },
+        addUnderTask:(state,{payload})=>{
+            const task=payload.task
+            const underTask=payload.underTask
+            console.log(task)
+            const id=state.indexOf(task)
+            console.log(id)
+            state[id].underTasks.push(underTask)
+        },
+        deleteTodo:(state,{payload})=>{
+            const id=state.indexOf(payload)
+            state.splice(id,1)
         }
     }
 })
 
 // Action creators are generated for each case reducer function
-export const {addTodo} = todosSlice.actions
+export const {addTodo,addUnderTask,deleteTodo} = todosSlice.actions
 
 export default todosSlice.reducer
