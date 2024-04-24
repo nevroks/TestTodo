05dcd8ac43aef5ecc7ac70569d57e356dc0376db
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-24 23:05:10 +0500

    added selected todos slice to persist blacklist

--- src/store/store.ts
+++ src/store/store.ts
@@ -24,7 +24,7 @@ const reducers = combineReducers(
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist:[""]
+    blacklist:["selectedTodos"]
 }
 const persistedReducers = persistReducer(persistConfig, reducers)
 
