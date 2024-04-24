7aa00ab70de20670f81f0eeaa8a9c1e90fc69d1c
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-24 23:02:21 +0500

    added logic of deleting todo

--- src/components/toolbar/ToolBar.tsx
+++ src/components/toolbar/ToolBar.tsx
@@ -4,10 +4,28 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
 import NoteIcon from '@mui/icons-material/Note';
 import {useAppDispatch} from "../../hooks/ReduxHooks.ts";
+import {useAppDispatch, useAppSelector} from "../../hooks/ReduxHooks.ts";
+import {changeTodo, deleteTodo} from "../../store/slices/todosSlice.ts";
 const ToolBar = () => {
     const dispatch=useAppDispatch()
 
     const handleDelete=()=>{
+        for (let todo of todos){
+            dispatch(deleteTodo(todo))
+        }
+        dispatch(refreshSelectedTodo())
+    }
+
+    const handleChange=()=>{
+        console.log(todos)
+        if (todos.length>1){
+            alert("Редактирование нескольких задач одновременно недоступно")
+            return
+        }
+        if (todos.length<1){
+            return
+        }
+        setIsTodoChanging(true)
 
     }
 
